c2eb23a7e450b817a4ad1b11ccb42aeb831a8d80
Sokolowski-Michal-Piotr <sokolowski.michal.piotr@gmail.com>
2018-12-21 09:49:37 +0100

    Added home controller.

---
+++ WebApi/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("")]
+        public IActionResult Index()
+        {
+            return Content("Hello world!");
+        }
+    }
+}
